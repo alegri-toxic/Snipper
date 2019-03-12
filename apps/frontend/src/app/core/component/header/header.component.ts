3a89cdbe42f05a8f30d050cd925570853ad22f09
Kortyka Thomas <tkortyka@alegri.eu>
2019-03-12 17:19:45 +0100

    added header class - dynamically

--- apps/frontend/src/app/core/component/header/header.component.ts
+++ apps/frontend/src/app/core/component/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from '../../../../environments/environment';
 
 @Component({
   selector: 'Snippeter-header',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+
+  public headerStyle: string = environment.ui.header;
 
   constructor() { }
 
