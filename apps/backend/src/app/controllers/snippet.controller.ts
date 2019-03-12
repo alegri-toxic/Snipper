b8c2853eedf94399068e19114dc1b31f07406985
Kortyka Thomas <tkortyka@alegri.eu>
2019-03-12 16:34:16 +0100

    debug output snippet controller

--- apps/backend/src/app/controllers/snippet.controller.ts
+++ apps/backend/src/app/controllers/snippet.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Query, Param } from '@nestjs/common';
+import { Controller, Get, Query, Param, Post, Body, Put, Delete } from '@nestjs/common';
 
 @Controller('snippets')
 export class SnippetController {
@@ -6,23 +6,69 @@ export class SnippetController {
   constructor() {}
 
   @Get()
-  getSnippets(@Query('query') query): void {
+  getSnippets(@Query('query') query): any {
     console.error(query);
+    return {
+      type: 'getSnippets',
+      query
+    };
   }
 
   @Get(':id')
-  getSnippet(@Query('query') query, @Param('id') id): void {
+  getSnippet(@Query('query') query, @Param('id') id): any {
     console.error(id, query);
+    return {
+      type: 'getSnippet',
+      id,
+      query
+    };
   }
 
   @Get('user/:id')
-  getSnippetByUser(@Query('query') query, @Param('id') id): void {
+  getSnippetByUser(@Query('query') query, @Param('id') id): any {
     console.error(id, query);
+    return {
+      type: 'getSnippetByUser',
+      userID: id,
+      query
+    };
   }
 
   @Get('tag/:id')
-  getSnippetByTag(@Query('query') query, @Param('id') id): void {
+  getSnippetByTag(@Query('query') query, @Param('id') id): any {
     console.error(id, query);
+    return {
+      type: 'getSnippetByTag',
+      tag: id,
+      query
+    };
+  }
+
+  @Post()
+  createSnippet(@Body() body): any {
+    console.error(body);
+    return {
+      type: 'createSnippet',
+      data: body
+    };
+  }
+
+  @Put(':id')
+  updateSnippet(@Param('id') id, @Body() body): any {
+    console.error(id, body);
+    return {
+      type: 'updateSnippet',
+      data: body
+    };
+  }
+
+  @Delete(':id')
+  deleteSnippet(@Param('id') id): any {
+    console.error(id);
+    return {
+      type: 'deleteSnippet',
+      id
+    };
   }
 
 }
